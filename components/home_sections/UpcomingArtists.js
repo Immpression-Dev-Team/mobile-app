eee94c8d89e382200822ce64369c9e50d4859bc0
Mahmoud <tkhattab1999@gmail.com>
2024-05-23 11:23:57 -0400

    Home page sections

---
+++ components/home_sections/UpcomingArtists.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+
+const imagePaths = [
+  require('../../assets/photos/path.jpg'),
+  require('../../assets/photos/animal.jpg'),
+  require('../../assets/photos/sunset.jpg'),
+  require('../../assets/photos/deer.jpg'),
+  require('../../assets/art/art1.jpg'),
+  require('../../assets/art/art2.png'),
+  require('../../assets/art/art3.png'),
+  require('../../assets/art/art4.png'),
+  require('../../assets/art/art5.png'),
+  require('../../assets/art/art6.png'),
+  require('../../assets/art/art7.png'),
+  require('../../assets/art/art8.png'),
+  require('../../assets/photos/building.jpg'),
+  require('../../assets/photos/man.jpg'),
+  require('../../assets/photos/hand.jpg'),
+  require('../../assets/photos/gray.jpg'),
+  // Add more images if needed
+];
+
+const chunkArray = (arr, chunkSize) => {
+  const chunks = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    chunks.push(arr.slice(i, i + chunkSize));
+  }
+  return chunks;
+};
+
+const UpcomingArtists = () => {
+  const imageChunks = chunkArray(imagePaths, 3); // Chunk into groups of 3 images
+
+  return (
+    <View style={styles.section}>
+      <Text style={styles.header}>Upcoming artists</Text>
+      <ScrollView horizontal style={styles.scrollView}>
+        {imageChunks.map((chunk, chunkIndex) => (
+          <View key={chunkIndex} style={styles.column}>
+            {chunk.map((path, index) => (
+              <Image
+                key={index}
+                source={path}
+                style={styles.image}
+              />
+            ))}
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  section: {
+    marginBottom: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  scrollView: {
+    flexDirection: 'row',
+  },
+  column: {
+    marginRight: 10, // Margin between columns
+  },
+  image: {
+    width: 110,
+    height: 110,
+    marginBottom: 10, // Margin between images in a column
+    borderRadius: 10,
+  },
+});
+
+export default UpcomingArtists;
--- screens/Home.js
+++ screens/Home.js
@@ -1,71 +1,21 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView, Image, TouchableOpacity, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import NavBar from '../components/Navbar';
 import LoginButton from '../components/LoginButton';
-
-// Define an array of image paths
-const imagePaths = [
-  require('../assets/art/art1.jpg'),
-  require('../assets/art/art2.png'),
-  require('../assets/art/art3.png'),
-  require('../assets/art/art4.png'),
-  require('../assets/art/art5.png'),
-  require('../assets/art/art6.png'),
-  require('../assets/art/art7.png'),
-  require('../assets/art/art8.png'),
-  require('../assets/photos/mountain.jpg'),
-  require('../assets/photos/grass.jpg'),
-  require('../assets/photos/building.jpg'),
-  require('../assets/photos/man.jpg'),
-  require('../assets/photos/hand.jpg'),
-  require('../assets/photos/gray.jpg'),
-  require('../assets/photos/path.jpg'),
-  require('../assets/photos/animal.jpg'),
-  require('../assets/photos/sunset.jpg'),
-  require('../assets/photos/deer.jpg'),
-  // Add more image paths as needed
-];
-
-const chunkArray = (arr, chunkSize) => {
-  const chunks = [];
-  for (let i = 0; i < arr.length; i += chunkSize) {
-    chunks.push(arr.slice(i, i + chunkSize));
-  }
-  return chunks;
-};
+import ArtForYou from '../components/home_sections/ArtForYou';
+import FeaturedArtists from '../components/home_sections/FeaturedArtists';
+import UpcomingArtists from '../components/home_sections/UpcomingArtists';
 
 const HomeScreen = () => {
-  const imageRows = chunkArray(imagePaths, 3);
-  const navigation = useNavigation();
-
-  const navigateTo = (screenName) => {
-    handleCloseSearch(); // Close search bar if open
-    navigation.navigate(screenName);
-  };
-
   return (
     <View style={styles.container}>
       <NavBar style={styles.navbar} />
-      {/* <Text>Welcome to Immpression LLC</Text> */}
-      <ScrollView horizontal>
-        <View style={styles.columnsContainer}>
-          {imageRows.map((row, rowIndex) => (
-            <View key={rowIndex} style={styles.column}>
-              {row.map((path, index) => (
-                <Image
-                  key={index}
-                  source={path}
-                  style={styles.image}
-                />
-              ))}
-            </View>
-          ))}
-          {/* Empty view to create space at the bottom */}
-          <View style={styles.bottomSpace} />
-        </View>
+      <ScrollView>
+        <ArtForYou />
+        <FeaturedArtists />
+        <UpcomingArtists />
+        <LoginButton />
       </ScrollView>
-      <LoginButton />
     </View>
   );
 };
@@ -73,30 +23,16 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  columnsContainer: {
-    flexDirection: 'row',
-    marginTop: 20,
-    marginLeft: 10,
-  },
-  column: {
-    marginRight: 10, // Margin between columns
-  },
-  image: {
-    width: 110,
-    height: 110,
-    marginBottom: 10, // Margin between images in a column
-    borderRadius: 10,
+    backgroundColor: 'white',
+    paddingHorizontal: 10,
+    paddingTop: 20,
   },
   navbar: {
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    zIndex: 1000, // Ensure the navbar is above the images
-  },
-  bottomSpace: {
-    height: 20, // Adjust the height as needed for space at the bottom
+    zIndex: 1000,
   },
 });
 
