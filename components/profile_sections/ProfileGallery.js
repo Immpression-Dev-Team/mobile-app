f4f0d11a0da93c1d8e529721f7c923be63519f1f
Mahmoud <tkhattab1999@gmail.com>
2024-06-13 17:48:45 -0400

    centered gallery images

--- components/profile_sections/ProfileGallery.js
+++ components/profile_sections/ProfileGallery.js
@@ -11,24 +11,32 @@ const images = [
 
 const ProfileGallery = () => {
     return (
-        <ScrollView contentContainerStyle={styles.galleryContainer}>
-            {images.map((image, index) => (
-                <Image key={index} source={image} style={styles.image} />
-            ))}
-        </ScrollView>
+        <View style={styles.all}>
+            <ScrollView contentContainerStyle={styles.galleryContainer}>
+                {images.map((image, index) => (
+                    <Image key={index} source={image} style={styles.image} />
+                ))}
+            </ScrollView>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
+    all: {
+        width: '100%',
+        alignItems: 'center',
+    },
     galleryContainer: {
+        width: '100%', // Adjust this width to control the gallery width
         flexDirection: 'row',
         flexWrap: 'wrap',
-
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
     },
     image: {
-        width: 100,
-        height: 100,
-        margin: 5,
+        width: 125,
+        height: 125,
+        margin: 1,
     },
 });
 
