250d643e0e6da5b5eb2b29e4232c18c0346b838e
Mahmoud <tkhattab1999@gmail.com>
2024-07-02 13:42:13 -0400

    Update ImageScreen.js

--- components/ImageScreen.js
+++ components/ImageScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 const ImageScreen = ({ route, navigation }) => {
-  const { image, artistName } = route.params;
+  const { image, artistName, artTitle } = route.params;
 
   return (
     <View style={styles.container}>
@@ -10,6 +10,7 @@ const ImageScreen = ({ route, navigation }) => {
         <Text style={styles.closeButtonText}>X</Text>
       </TouchableOpacity>
       <Image source={image} style={styles.fullImage} />
+      <Text style={styles.artTitle}>{artTitle}</Text>
       <Text style={styles.artistName}>{artistName}</Text>
     </View>
   );
@@ -18,29 +19,34 @@ const ImageScreen = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
   },
   closeButton: {
     position: 'absolute',
-    top: 30,
+    top: 50,
     right: 20,
     zIndex: 1,
   },
   closeButtonText: {
-    color: 'white',
+    color: 'black',
     fontSize: 24,
   },
   fullImage: {
-    width: '100%',
-    height: '80%',
+    width: 350,
+    height: 400,
     resizeMode: 'contain',
   },
-  artistName: {
-    color: 'white',
-    fontSize: 18,
+  artTitle: {
+    color: 'black',
+    fontSize: 20,
     marginTop: 10,
+  },
+  artistName: {
+    color: 'black',
+    fontSize: 18,
+    marginTop: 5,
   },
 });
 
