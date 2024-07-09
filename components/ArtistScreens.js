a4c97f9908effd8c20c8edbf1b7a4523423bbab4
Mahmoud <tkhattab1999@gmail.com>
2024-07-08 20:42:02 -0400

    artist screen

---
+++ components/ArtistScreens.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const ArtistScreen = ({ route }) => {
+    const navigation = useNavigation();
+    const { artist, profilePic } = route.params;
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.closeButton} onPress={() => navigation.goBack()}>
+                <Text style={styles.closeButtonText}>X</Text>
+            </TouchableOpacity>
+            <View style={styles.card}>
+                <Text style={styles.artistName}>{artist}</Text>
+                <Image source={profilePic} style={styles.image} />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        paddingHorizontal: 20,
+    },
+    closeButton: {
+        position: 'absolute',
+        top: 40,
+        right: 20,
+        backgroundColor: 'red',
+        borderRadius: 15,
+        width: 30,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1,
+    },
+    closeButtonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    card: {
+        width: '90%',
+        backgroundColor: 'gray',
+        borderRadius: 10,
+        padding: 5,
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    image: {
+        width: '100%', // Make image width 100% of the card width
+        height: 400,
+        borderRadius: 0,
+        marginTop: 10, // To provide some space between the artist name and the image
+    },
+    artistName: {
+        fontSize: 24,
+        marginTop: 10,
+        color: 'white',
+        fontWeight: 'bold',
+        alignSelf: 'flex-start',
+        marginLeft: 10,
+    },
+});
+
+export default ArtistScreen;
