8614f9ff607420ab5715db3da8004ecea0fbd454
Mahmoud <tkhattab1999@gmail.com>
2024-08-08 21:44:02 -0400

    Navbar background

--- components/FooterNavbar.js
+++ components/FooterNavbar.js
@@ -1,33 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const FooterNavbar = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
-        <Image source={require('../assets/icons/Home.png')} style={styles.icon} />
-        {/* <Text style={styles.text}>Home</Text> */}
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Upload')}>
-        <Image source={require('../assets/icons/Upload.png')} style={styles.icon} />
-        {/* <Text style={styles.text}>Upload</Text> */}
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Profile')}>
-        <Image source={require('../assets/icons/Profile.png')} style={styles.icon} />
-        {/* <Text style={styles.text}>Profile</Text> */}
-      </TouchableOpacity>
-    </View>
+    <ImageBackground 
+      source={require('../assets/backgrounds/footer-bg.png')} // Replace with your image path
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
+          <Image source={require('../assets/icons/Home.png')} style={styles.icon} />
+          {/* <Text style={styles.text}>Home</Text> */}
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Upload')}>
+          <Image source={require('../assets/icons/Upload.png')} style={styles.icon} />
+          {/* <Text style={styles.text}>Upload</Text> */}
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Profile')}>
+          <Image source={require('../assets/icons/Profile.png')} style={styles.icon} />
+          {/* <Text style={styles.text}>Profile</Text> */}
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    width: '100%',
+    height: 70,
+  },
   container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    backgroundColor: '#EBEBEB',
+    backgroundColor: 'transparent', // Set backgroundColor to transparent
     borderTopWidth: 0.2,
     borderTopColor: '#ADBBD6',
     paddingBottom: 20,
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, ImageBackground } from 'react-native';
 import Navbar from '../components/Navbar';
 import LoginButton from '../components/LoginButton';
 import ArtForYou from '../components/home_sections/ArtForYou';
@@ -12,37 +12,46 @@ import Categories from '../components/home_sections/Categories';
 import InviteFriends from '../components/home_sections/InviteFriends';
 import FooterNavbar from '../components/FooterNavbar';
 
-// TODO conditionally render if the user is logged in or not (remove the login button when logged in and show the logout button instead)
 const HomeScreen = () => {
   return (
-    <View style={styles.everything}>
-      <View style={styles.navbar}>
-        <Navbar />
-        {/* <LongSearchBar /> */}
-      </View>
-      <ScrollView>
-        <View style={styles.container}>
-          <ArtForYou />
-          {/* <ArtQuotes /> */}
-          {/* <FillerMessage /> */}
-          <InviteFriends />
-          <Categories />
-          <FeaturedArtists />
-          <ArtistsPick />
-          {/* this is the original login button and will fix later */}
-          {/* <LoginButton /> */}
+    <ImageBackground 
+      source={require('../assets/backgrounds/navbar-bg.png')} // Replace with your image path
+      style={styles.backgroundImage}
+    >
+      <View style={styles.everything}>
+        <View style={styles.navbar}>
+          <Navbar />
+          {/* <LongSearchBar /> */}
         </View>
-      </ScrollView>
-      <View style={styles.footer}>
-        <FooterNavbar />
+        <ScrollView>
+          <View style={styles.container}>
+            <ArtForYou />
+            {/* <ArtQuotes /> */}
+            {/* <FillerMessage /> */}
+            <InviteFriends />
+            <Categories />
+            <FeaturedArtists />
+            <ArtistsPick />
+            {/* this is the original login button and will fix later */}
+            {/* <LoginButton /> */}
+          </View>
+        </ScrollView>
+        <View style={styles.footer}>
+          <FooterNavbar />
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    width: '100%',
+    height: 124,
+    flex: 1,
+  },
   everything: {
-    backgroundColor: '#EBEBEB',
+    backgroundColor: 'transparent', // Set backgroundColor to transparent
     flex: 1,
   },
   container: {
