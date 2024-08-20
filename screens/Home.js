c0d13a37ecd3db25746ef0ed04058d36b192ea32
Mahmoud <tkhattab1999@gmail.com>
2024-08-19 21:07:20 -0400

    extra space on home removed

--- screens/Home.js
+++ screens/Home.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    paddingBottom: 300,
+    paddingBottom: 10,
     paddingHorizontal: 0,
     alignItems: "center", // Center horizontally
     backgroundColor: "#fff",
