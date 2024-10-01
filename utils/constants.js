73451e382636f6146d9ca086abf1f7623395db41
Richard Ulysse <Rchrdlss3@Gmail.com>
2024-10-01 19:19:39 -0400

    feat(helpers.js): refacted

--- utils/constants.js
+++ utils/constants.js
@@ -1,28 +1,18 @@
-import { lazy } from 'react'
 import SettingsScreen from '../screens/Settings'
 import HomeScreen from '../screens/Home'
 import StatisticsScreen from '../screens/Statistics'
 import Profile from '../screens/Profile'
-// const HomeScreen = lazy(() => import('../screens/Home'))
-// const StatisticsScreen = lazy(() => import ('../screens/Statistics'))
-// const ProfileScreen = lazy(() => import ('../screens/Profile'))
-// const SettingsScreen = lazy(() => import ('../screens/Settings'))
 import SignUp from '../screens/SignUp'
 import ImageScreen from '../components/ImageScreen'
 import PasswordReset from '../screens/PasswordReset'
 import ArtistScreen from '../components/ArtistScreens'
 import Upload from '../screens/Upload'
 import Login from '../screens/Login'
-// const SignUp = lazy(() => import ('../screens/SignUp'))
-// const ImageScreen = lazy(() => import ('../components/ImageScreen'))
-// const PasswordReset = lazy(() => import ('../screens/PasswordReset'))
-// const ArtistScreen = lazy(() => import ('../components/ArtistScreens'))
-// const Upload = lazy(() => import ('../screens/Upload'))
-// const Login = lazy(() => import ('../screens/Login'))
+
 
 export const UserNavigation = [{
     name: 'Home',
-    component: HomeScreen
+    component: HomeScreen 
 },
 {
     name: 'Statisctics',
@@ -60,7 +50,7 @@ export const UserNavigation = [{
 export const GuestNavigation = [
     {
         name:'Login',
-        component: Login
+        component: Login 
     },
     {
         name:'SignUp',
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,18 @@
-import { Suspense } from "react";
 import { GuestNavigation, UserNavigation } from "./constants"
 import { createStackNavigator } from "@react-navigation/stack";
-import { ActivityIndicatorComponent } from "react-native";
 
 const Stack = createStackNavigator();
 
+/* 
+For lazy loading. Currently not working, pushing for code refactor.
 export const pageSuspense = (component) => {
-    return <Suspense fallback = {<ActivityIndicatorComponent/>}>{component}</Suspense>
+//     return (
+//     <Suspense fallback = {<ActivityIndicatorComponent/>}>
+//         {component}
+//     </Suspense>
+//     )
 }
+*/ 
 
 export const userStackScreen = () => {
     return (
