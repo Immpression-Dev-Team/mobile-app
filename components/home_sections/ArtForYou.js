af5b01f5282cb662fd84e4ca2d2ed29e6a4e2a64
Sagar Gopalani <sgopalani12@gmail.com>
2024-10-26 23:08:51 -0400

    style(artforyou.js): got rid of whtite block above loading gif

--- components/home_sections/ArtForYou.js
+++ components/home_sections/ArtForYou.js
@@ -272,7 +272,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
   },
   loadingGif: {
     width: 100,
