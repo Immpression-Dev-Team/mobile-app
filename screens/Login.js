eabe2563e30a8360c5268a2b3509091480680147
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-10-30 11:45:57 +0100

    feat(SignUp.js): enhance signup process and error handling
    - Fixed issue with signup button not redirecting upon successful account creation.
    - Implemented loading state animation for the signup button.
    - Consolidated error handling into a single state for improved readability and maintainability.
    - Added user feedback for various signup validation errors.
    - Enhanced error display with color and spacing adjustments for better visibility.

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
@@ -8,30 +8,43 @@ import {
   Pressable,
   Image,
   ImageBackground, // Import ImageBackground component
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import axios from "axios";
-import { API_URL } from "../config";
-import Icon from "react-native-vector-icons/FontAwesome"; // Import your preferred icon set
-import { handleLogin } from "../utils/handleLogin.js";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useAuth } from "../state/AuthProvider";
-import SignUp from "./SignUp";
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import { API_URL } from '../config';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Import your preferred icon set
+import { handleLogin } from '../utils/handleLogin.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useAuth } from '../state/AuthProvider';
+import SignUp from './SignUp';
 
-const logoImage = require("../assets/Logo_T.png"); // Adjust the path to your logo image
-const headerImage = require("../assets/headers/Immpression_multi.png"); // Adjust the path to your header image
-const backgroundImage = require("../assets/backgrounds/paint_background.png"); // Adjust the path to your background image
+const logoImage = require('../assets/Logo_T.png'); // Adjust the path to your logo image
+const headerImage = require('../assets/headers/Immpression_multi.png'); // Adjust the path to your header image
+const backgroundImage = require('../assets/backgrounds/paint_background.png'); // Adjust the path to your background image
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [userData, setUserData] = useState(null);
   const { login } = useAuth();
   const navigation = useNavigation();
+  const [error, setError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await handleLogin(email, password, setUserData, navigation, login);
+    const result = await handleLogin(
+      email,
+      password,
+      setUserData,
+      navigation,
+      login
+    );
+
+    if (result) {
+      navigation.navigate('Home');
+    } else {
+      setError('Invalid email or password');
+    }
   };
 
   const navigateTo = (screenName) => {
@@ -86,7 +99,16 @@ const Login = () => {
                 />
               </View>
               <Text
-                onPress={() => navigateTo("PasswordReset")}
+                style={{
+                  color: 'red',
+                  textAlign: 'center',
+                  marginTop: 10,
+                }}
+              >
+                {error && error}
+              </Text>
+              <Text
+                onPress={() => navigateTo('PasswordReset')}
                 style={styles.forgotPasswordText}
               >
                 Forgot Password?
@@ -97,7 +119,7 @@ const Login = () => {
                 <Text style={styles.buttonText}>Login</Text>
               </Pressable>
               <Pressable
-                onPress={() => navigateTo("SignUp")}
+                onPress={() => navigateTo('SignUp')}
                 style={[styles.button, styles.buttonOutline]}
               >
                 <Text style={styles.buttonOutlineText}>Sign Up</Text>
@@ -115,59 +137,59 @@ export default Login;
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
   container: {
     flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
+    justifyContent: 'flex-start',
+    alignItems: 'center',
     paddingTop: 0,
   },
   totalHeader: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
   },
   logoContainer: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 20,
   },
   logo: {
     width: 70,
     height: 70,
-    resizeMode: "contain",
+    resizeMode: 'contain',
   },
   headerImageContainer: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 10,
     marginLeft: 12,
   },
   headerImage: {
     width: 200,
     height: 50,
-    resizeMode: "contain",
+    resizeMode: 'contain',
   },
   contentContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
   },
   keyboardAvoidingContainer: {
-    width: "80%",
+    width: '80%',
   },
   inputContainer: {
-    width: "100%",
+    width: '100%',
     marginTop: -200, // Adjust this value to bring inputs higher up
   },
   inputWrapper: {
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#C6C7DE",
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#C6C7DE',
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 20,
@@ -181,39 +203,39 @@ const styles = StyleSheet.create({
     height: 40,
   },
   forgotPasswordText: {
-    textAlign: "center",
+    textAlign: 'center',
     marginTop: 10,
     marginRight: 10,
-    color: "#3C3D52",
-    textDecorationLine: "underline",
+    color: '#3C3D52',
+    textDecorationLine: 'underline',
   },
   buttonContainer: {
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 50, // Adjust this value to bring buttons higher up
   },
   button: {
-    backgroundColor: "blue",
-    width: "100%",
+    backgroundColor: 'blue',
+    width: '100%',
     padding: 11,
     borderRadius: 20,
     marginTop: 10,
   },
   buttonText: {
-    textAlign: "center",
-    color: "white",
-    fontWeight: "bold",
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
     fontSize: 15,
   },
   buttonOutline: {
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
     marginTop: 10,
   },
   buttonOutlineText: {
-    color: "black",
-    fontWeight: "bold",
+    color: 'black',
+    fontWeight: 'bold',
     fontSize: 15,
-    textAlign: "center",
+    textAlign: 'center',
   },
 });
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -7,36 +7,77 @@ import {
   Pressable,
   Image,
   ImageBackground, // Import ImageBackground component
-} from "react-native";
-import React, { useState } from "react";
-import NavBar from "../components/Navbar";
-import { API_URL } from "../config";
-import axios from "axios";
-import { useNavigation } from "@react-navigation/native";
-import { handleLogin } from "../utils/handleLogin";
-import Icon from "react-native-vector-icons/FontAwesome"; // Import your preferred icon set
-import { showToast } from "../utils/toastNotification";
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import NavBar from '../components/Navbar';
+import { API_URL } from '../config';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+import { handleLogin } from '../utils/handleLogin';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Import your preferred icon set
+import { showToast } from '../utils/toastNotification';
 
-const logoImage = require("../assets/Logo_T.png"); // Adjust the path to your logo image
-const headerImage = require("../assets/headers/Immpression_multi.png"); // Adjust the path to your header image
-const backgroundImage = require("../assets/backgrounds/babyBlue.png"); // Adjust the path to your background image
+const logoImage = require('../assets/Logo_T.png'); // Adjust the path to your logo image
+const headerImage = require('../assets/headers/Immpression_multi.png'); // Adjust the path to your header image
+const backgroundImage = require('../assets/backgrounds/babyBlue.png'); // Adjust the path to your background image
 
 const SignUp = () => {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   const [passwordLengthError, setPasswordLengthError] = useState(false);
-  const [userAlreadyExistsError, setUserAlreadyExistsError] = useState(false);
+  // const [userAlreadyExistsError, setUserAlreadyExistsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [ellipsis, setEllipsis] = useState('');
   //   const [confirmPassword, setConfirmPassword] = useState("")
 
+  // Error states
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [userAlreadyExistsError, setUserAlreadyExistsError] = useState(false);
+
+  const [error, setError] = useState('');
+
   const navigation = useNavigation();
+
+  // Animate loading state
+  useEffect(() => {
+    if (isLoading) {
+      const intervalId = setInterval(() => {
+        setEllipsis((prev) => (prev.length < 3 ? prev + '.' : ''));
+      }, 500); // Adjust the speed as desired
+
+      return () => clearInterval(intervalId); // Clear interval on unmount or stop loading
+    } else {
+      setEllipsis(''); // Reset ellipsis when not loading
+    }
+  }, [isLoading]);
+
+  // Simple email validation
+  const isValidEmail = (email) => /\S+@\S+\.\S+/.test(email);
 
   // TODO: set up frontend confirmPassword logic before calling handleSubmit
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (password.length < 6) {
-      setPasswordLengthError(true);
+    // Reset error state
+    setError('');
+
+    // Perform client-side validation and stop at the first failure
+    if (name.length < 4) {
+      setError('Name must be at least 4 characters long.');
+      return;
     }
+    if (!isValidEmail(email)) {
+      setError('Please enter a valid email address.');
+      return;
+    }
+    if (password.length < 6) {
+      setError('Password must be at least 6 characters long.');
+      return;
+    }
+
+    setIsLoading(true); // Start loading. block user from creating another request.
     try {
       const response = await axios.post(`${API_URL}/signup`, {
         name,
@@ -45,23 +86,23 @@ const SignUp = () => {
       });
       if (response.data.success) {
         await handleLogin(email, password, navigation);
-        // navigation.navigate('Home');
+        navigation.navigate('Login');
       } else {
-        console.log("Signup failed");
-        showToast("Signup Failed");
+        console.log('Signup failed');
+        showToast('Signup Failed');
       }
     } catch (err) {
-      showToast("Error during login");
-      console.log("Error during login:", err);
-      if (err.response.data.error.includes("User already exists")) {
-        setUserAlreadyExistsError(true);
-      }
+      showToast('Error during login');
+      console.log('Error during login:', err.response.data.message);
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
     }
   };
   console.log(passwordLengthError);
 
   const handleBack = () => {
-    navigation.navigate("Login");
+    navigation.navigate('Login');
   };
 
   return (
@@ -127,21 +168,28 @@ const SignUp = () => {
                 </View>
               </View>
             </View>
-            <Text style={{ color: "red", textAlign: "center" }}>
+            <Text style={{ color: 'red', textAlign: 'center' }}>
               {passwordLengthError
-                ? "Password must be at least 6 characters"
-                : ""}
+                ? 'Password must be at least 6 characters'
+                : ''}
             </Text>
-            <Text style={{ color: "red", textAlign: "center" }}>
-              {userAlreadyExistsError ? "User already exists" : ""}
+            <Text style={{ color: 'red', textAlign: 'center' }}>
+              {error && error}
             </Text>
             {/* <TextInput placeholder="Confirm Password" value={confirmPassword} onChangeText={text => setConfirmPassword(text)} style={styles.input} secureTextEntry /> */}
           </View>
           <Pressable
             onPress={handleSubmit}
-            style={[styles.button, styles.buttonOutline]}
+            disabled={isLoading}
+            style={[
+              styles.button,
+              styles.buttonOutline,
+              isLoading && { opacity: 0.7 },
+            ]}
           >
-            <Text style={styles.buttonOutlineText}>Sign Up</Text>
+            <Text style={styles.buttonOutlineText}>
+              {isLoading ? `Signing Up${ellipsis}` : 'Sign Up'}
+            </Text>
           </Pressable>
           <Pressable
             onPress={handleBack}
@@ -163,31 +211,31 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
   signUpContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingHorizontal: 20,
     paddingTop: 20, // Adjust the top padding to create space between NavBar and Sign Up section
   },
   title: {
     fontSize: 30,
     marginBottom: 30,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 
   inputContainer: {
-    width: "90%",
+    width: '90%',
     marginTop: 0, // Adjust this value to bring inputs higher up
   },
   inputWrapper: {
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#C6C7DE",
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#C6C7DE',
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 20,
@@ -202,64 +250,64 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     // flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
   },
   button: {
-    backgroundColor: "blue",
-    width: "80%",
+    backgroundColor: 'blue',
+    width: '80%',
     padding: 10,
     borderRadius: 10,
     marginTop: 20,
   },
   buttonOutline: {
-    backgroundColor: "blue",
+    backgroundColor: 'blue',
     marginTop: 10,
     borderRadius: 20,
   },
   buttonOutline2: {
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
     marginTop: 10,
     borderRadius: 20,
   },
   buttonOutlineText: {
-    color: "white",
-    fontWeight: "bold",
+    color: 'white',
+    fontWeight: 'bold',
     fontSize: 15,
-    textAlign: "center",
+    textAlign: 'center',
   },
   buttonOutlineText2: {
-    color: "black",
-    fontWeight: "bold",
+    color: 'black',
+    fontWeight: 'bold',
     fontSize: 15,
-    textAlign: "center",
+    textAlign: 'center',
   },
   logoContainer: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 20,
   },
   logo: {
     width: 70,
     height: 70,
-    resizeMode: "contain",
+    resizeMode: 'contain',
   },
   headerImageContainer: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 10,
     marginLeft: 12,
   },
   headerImage: {
     width: 200,
     height: 50,
-    resizeMode: "contain",
+    resizeMode: 'contain',
   },
   totalHeader: {
     // flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
   },
 });
