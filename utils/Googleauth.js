b819745aea9b1b782f4a036234800c28923291a0
Mahmoud <tkhattab1999@gmail.com>
2024-11-22 20:12:41 -0500

    chore(googleauth.js): add credentials

--- utils/Googleauth.js
+++ utils/Googleauth.js
@@ -1,8 +1,8 @@
 export const googleConfig = {
-    expoClientId: "31361740434-f1lrp7btuc35vupc4mdnls5kd0i6ae8d.apps.googleusercontent.com",
-    iosClientId: "31361740434-uasqvn754uln5hgcijp6n59lc4ig4s74.apps.googleusercontent.com",
-    webClientId: "31361740434-f1lrp7btuc35vupc4mdnls5kd0i6ae8d.apps.googleusercontent.com",
-    androidClientId: "31361740434-r2bk04trpif4jfq6ej8aogdkepdnobmi.apps.googleusercontent.com",
+    expoClientId: "580547124298-c9lkii607bb9e9537nl7kb1l4umi341h.apps.googleusercontent.com",
+    iosClientId: "580547124298-uqf20p2tfe66bti4lrot2tr2tp0a8d6s.apps.googleusercontent.com",
+    webClientId: "580547124298-c9lkii607bb9e9537nl7kb1l4umi341h.apps.googleusercontent.com",
+    androidClientId: "580547124298-kn4i5c50j7drifr14q0ou91uncjiuvq3.apps.googleusercontent.com",
     scopes: ['profile', 'email'],
-    redirectUri: "com.googleusercontent.apps.31361740434-uasqvn754uln5hgcijp6n59lc4ig4s74:/oauth2redirect"
+    redirectUri: "com.googleusercontent.apps.580547124298-uqf20p2tfe66bti4lrot2tr2tp0a8d6s"
 };
