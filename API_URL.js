159ec1291c1b389f7210f830e469dced67c0a95e
Premo <ajaipremo@gmail.com>
2025-03-15 12:48:42 -0400

    fix(repo): accidentally deletion of branch

--- API_URL.js
+++ API_URL.js
@@ -1,13 +1,13 @@
+import dotenv from "dotenv"
+
 // Change between production and development backend services
 // "dev" for local OR "prod" for remote
 const ENV = "prod";
 
+dotenv.config();
 
-// TODO: REPLACE WITH *YOUR* HOST IP "192.1.x.x" for most local networks. You must also add your ip in the root .env file
-// Linux run - ip addr
-// Windows run - ipconfig
-// Mac run - ifconfig | grep "inet "
-const HOST_IP = "192.168.1.6";
+// TODO: ADD IP TO .env file located in mobile-app folder
+const HOST_IP = process.env.HOST_IP;
 
 const PROD_URL = "https://immpression-backend.vercel.app"
 const API_URL =
